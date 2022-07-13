1fbef0d3c52916cfdabb5059f679e47d7c9fa098
user <evgeniiperkin@bk.ru>
2022-07-13 16:31:03 +0300

    change view

--- starknet.sh
+++ starknet.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 # Default variables
-cd ~/pathfinder
-git fetch
-git checkout v0.2.5-alpha
-cargo build --release --bin pathfinder
-mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
-cd py
-source .venv/bin/activate
-PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
-systemctl restart starknetd
-pathfinder -V
+function=main
+C_LGn='\033[1;32m'
+C_R='\033[0;31m'
+RES='\033[0m'
+
+printf_n(){ printf "$1\n" "${@:2}"; }
+
+main(){
+    . <(wget -qO- https://raw.githubusercontent.com/EvgeniiPerkin/scripts/main/logo.sh)
+    cd ~/pathfinder
+    git fetch
+    git checkout v0.2.5-alpha
+    cargo build --release --bin pathfinder
+    mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+    cd py
+    source .venv/bin/activate
+    PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+    systemctl restart starknetd
+    local version=pathfinder -V
+    printf_n "${C_LGn}$version${RES}"
+    printf_n "${C_LGn}Done.${RES}"
+}
+
+$function
