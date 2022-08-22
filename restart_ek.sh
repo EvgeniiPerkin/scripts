0d282c458cf329828fef2f739b19e8421895c7d8
user <evgeniiperkin@bk.ru>
2022-08-22 09:41:49 +0300

    experement for solana

--- restart_ek.sh
+++ restart_ek.sh
@@ -8,18 +8,24 @@ RES='\033[0m'
 printf_n(){ printf "$1\n" "${@:2}"; }
 
 main() {
+    if [[ $1 == "" ]]; then
+        printf_n "${C_R}You have not specified a directory for the ledger!!!${RES}"
+        return 0
+    fi
+    printf_n $1
     . <(wget -qO- https://raw.githubusercontent.com/EvgeniiPerkin/scripts/main/logo.sh)
-    printf_n "${C_LGn}Stop solana service...${RES}"
-    systemctl stop solana
-    cd /root/solana/solana-snapshot-finder/
-    printf_n "${C_LGn}rm ledger...${RES}"
-    rm -rf /mnt/ledger/*
-    printf_n "${C_LGn}work snapshot-finder...${RES}"
-    
-    python3 snapshot-finder.py --snapshot_path /mnt/ledger -r http://api.testnet.solana.com --min_download_speed 5
-    printf_n "${C_LGn}start solana...${RES}"
-    systemctl start solana
-    printf_n "${C_LGn}Done...${RES}"
+    #printf_n "${C_LGn}Stop solana service...${RES}"
+    #systemctl stop solana
+    #cd /root/solana/solana-snapshot-finder/
+    #printf_n "${C_LGn}rm ledger...${RES}"
+    #rm -rf $1/*
+    #printf_n "${C_LGn}work snapshot-finder...${RES}"
+
+    #python3 snapshot-finder.py --snapshot_path $1 -r http://api.testnet.solana.com --min_download_speed 5
+    #printf_n "${C_LGn}start solana...${RES}"
+    #systemctl start solana
+    #printf_n "${C_LGn}Done...${RES}"
+    #wget -qO- eth0.me
 }
 
 $function
