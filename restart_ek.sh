ab8b2eb8590d56d099a840e049bc4e8d518c0dba
user <evgeniiperkin@bk.ru>
2022-08-22 09:11:45 +0300

    change restart ek script

--- restart_ek.sh
+++ restart_ek.sh
@@ -9,10 +9,14 @@ printf_n(){ printf "$1\n" "${@:2}"; }
 
 main() {
     . <(wget -qO- https://raw.githubusercontent.com/EvgeniiPerkin/scripts/main/logo.sh)
+    printf_n "${C_LGn}Stop solana service...${RES}"
     systemctl stop solana
     cd /root/solana/solana-snapshot-finder/
+    printf_n "${C_LGn}rm ledger...${RES}"
     rm -rf /mnt/ledger/ledger/*
+    printf_n "${C_LGn}work snapshot-finder...${RES}"
     python3 snapshot-finder.py --snapshot_path /mnt/ledger/ledger -r http://api.testnet.solana.com --min_download_speed 5
+    printf_n "${C_LGn}start solana...${RES}"
     systemctl start solana
     printf_n "${C_LGn}Done...${RES}"
 }
