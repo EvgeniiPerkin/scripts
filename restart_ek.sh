d98143dd0ccb6964800f34690f166aa0a317dce4
user <evgeniiperkin@bk.ru>
2022-08-22 09:48:11 +0300

    experiment 2

--- restart_ek.sh
+++ restart_ek.sh
@@ -5,6 +5,34 @@ C_LGn='\033[1;32m'
 C_R='\033[0;31m'
 RES='\033[0m'
 
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/EvgeniiPerkin/scripts/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: script for restarting the solana node with the removal of the messenger"
+		echo
+		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES} ${C_R}/root/solana/ledger${RES}"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+        echo -e "  -h,   --help                         show the help page"
+        echo -e "  -dm,  --delete-ledger-main           delete the ledger and run the fender on the main network"
+        echo -e "  -m,   --main                         run the fender on the main network"
+        echo -e "  -dt,  --delete-ledger-test           delete the ledger and run the fender on the test network"
+        echo -e "  -t,   --test                         run the fender on the test network"
+		echo
+		return 0
+		;;
+	-k|--koks)
+		function="main $2"
+		shift
+		;;
+	*|--)
+		break
+		;;
+	esac
+done
+
 printf_n(){ printf "$1\n" "${@:2}"; }
 
 main() {
