7a95a7ccf99a12d79e77e6e82372c9bc233ab3d9
user <evgeniiperkin@bk.ru>
2022-08-31 09:11:18 +0300

    update script sui

--- sui.sh
+++ sui.sh
@@ -10,8 +10,11 @@ cd sui
 git remote add upstream https://github.com/MystenLabs/sui
 git fetch upstream
 git checkout -B devnet --track upstream/devnet
-cargo build --release
+cargo build -p sui-node -p sui --release
 mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+cp /usr/local/bin/sui-node $(which sui-node)
+cp /usr/local/bin/sui $(which sui)
 systemctl restart suid
+sui -V
