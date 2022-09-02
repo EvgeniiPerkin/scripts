f495e19564f154c1606919aba3eea772c6ab7b43
user <evgeniiperkin@bk.ru>
2022-09-02 17:12:57 +0300

    update starknet script

--- starknet.sh
+++ starknet.sh
@@ -11,7 +11,7 @@ main(){
     . <(wget -qO- https://raw.githubusercontent.com/EvgeniiPerkin/scripts/main/logo.sh)
     cd ~/pathfinder
     git fetch
-    git checkout v0.3.0-alpha
+    git checkout v0.3.2
     cargo build --release --bin pathfinder
     mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
     cd py
