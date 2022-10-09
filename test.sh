3cb18b73e48005f7e05820943e864976ed2f49a6
user <evgeniiperkin@bk.ru>
2022-10-09 11:37:49 +0300

    change test script

--- test.sh
+++ test.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 # Default variables
-read -p 'Username Firstname: ' uservar namevar
-read -sp 'Password: ' passvar
-echo
-tee <<EOF >/dev/null /home/user/work/service.test
-[Unit]
-Description=Massa Node
-After=network-online.target
 
-[Service]
-User=$uservar
-WorkingDirectory='$namevar'
-ExecStart="$namevar"
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-pass=$passvar
-
-[Install]
-WantedBy=multi-user.target
-EOF
+touch temp_01.txt
