7b375d36254e76c82de8cb18bf79fc9f42330be7
user <evgeniiperkin@bk.ru>
2022-10-13 11:17:54 +0300

    test solana ledger path

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 # Default variables
 
-touch temp_01.txt
+path_ledger=""
+
+while IFS= read -r line
+do
+    if [[ "$line" == *"--ledger"* ]]; then
+        path_ledger="$line"
+        path_ledger=${path_ledger#-*r}
+        l=${#path_ledger}-3
+        path_ledger=${path_ledger:1:$l}
+        echo ${#path_ledger}
+        echo $path_ledger
+    fi
+done < /root/solana/solana.service
