a1061ced648fc794e6f32bd3dee6dacaf518b17d
user <evgeniiperkin@bk.ru>
2022-10-25 09:04:58 +0300

    update sui script

--- update_db_sui.sh
+++ update_db_sui.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 systemctl stop suid
-rm -rf /var/sui/db/* /var/sui/genesis.blob
+rm -rf /var/sui/db/*
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 systemctl restart suid
