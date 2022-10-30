7d55f009052e07032cb8fef20629eaed163b2cd2
user <evgeniiperkin@bk.ru>
2022-10-30 09:12:16 +0300

    update sui script

--- sui.sh
+++ sui.sh
@@ -2,20 +2,15 @@
 # Default variables
 
 systemctl stop suid
-rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
 source $HOME/.cargo/env
-cd $HOME
-git clone https://github.com/MystenLabs/sui.git
-cd sui
-git remote add upstream https://github.com/MystenLabs/sui
+rm -rf $HOME/.sui/db
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+cd $HOME/sui
 git fetch upstream
+git stash
 git checkout -B devnet --track upstream/devnet
-cargo build -p sui-node -p sui --release
-mv ~/sui/target/release/sui-node /usr/local/bin/
-mv ~/sui/target/release/sui /usr/local/bin/
-wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-cp /usr/local/bin/sui-node $(which sui-node)
-cp /usr/local/bin/sui $(which sui)
+cargo build --release
+mv $HOME/sui/target/release/{sui,sui-node,sui-faucet} /usr/bin/
 systemctl restart suid
 sui -V
 wget -qO- eth0.me
