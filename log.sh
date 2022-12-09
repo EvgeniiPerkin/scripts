7ac0a5029b9466f44c46e7a7769604c9a240c8c3
evgenii <evgeniiperkin@bk.ru>
2022-12-09 16:11:31 +0300

    save changed

--- log.sh
+++ log.sh
@@ -4,12 +4,13 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>out.log 2>&1
 
+SOLANA_PATH="$HOME/.local/share/solana/install/active_release/bin/solana"
+
 log() {
     echo "$(date) : $1" >&1
 }
 
 main() {
-    log "hello"
-    log "how are you"
+    log "test"
 }
 $function
